095cc0483a3c5c72063b746476de8690c9a8d54b
PabloCampusano <campusanopablo990@gmail.com>
2025-07-04 17:18:32 -0400

    1
    comic1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,16 @@
 "use client"
 import { useEffect, useState } from "react";
+
+type Evento = {
+  nombreEvento: string
+  asistentes: number
+  tipoEvento: string
+  descripcion: string
+  fecha: string
+}
+
+
+
 export default function Home() {
   return (
     <div>

571ed42c5a4b96676eae1e9acdbc0a58c50c9583
PabloCampusano <campusanopablo990@gmail.com>
2025-07-04 18:21:37 -0400

    comic2
    2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,19 +2,33 @@
 import { useEffect, useState } from "react";
 
 type Evento = {
-  nombreEvento: string
+  nombre: string
   asistentes: number
-  tipoEvento: string
+  tipo: string
   descripcion: string
   fecha: string
 }
 
+const eventoInicial: Evento = { 
+  nombre: "",
+  asistentes: 0,
+  tipo:"",
+  descripcion: "",
+  fecha:""
 
+}
 
 export default function Home() {
+  const [evento, setEvento] = useState(eventoInicial)
+  const [eventoAEditar, setEventoAEditar] = useState(eventoInicial)
+  const [eventos, setEventos] = useState<Evento[]>([])
+  const [modoEdicion, setModoEdicion] = useState(false)
   return (
     <div>
       <h1>hola</h1>
     </div>
   );
 }
+
+
+
